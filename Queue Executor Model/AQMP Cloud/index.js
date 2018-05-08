cc0582877482c4296545bc8945db3063d25cdcfb
MF57 <p-bochenek@outlook.com>
2018-05-08 15:10:52 +0200

    run configurations for gcf and aws

--- Queue Executor Model/AQMP Cloud/index.js
+++ Queue Executor Model/AQMP Cloud/index.js
@@ -2,7 +2,25 @@ const amqp = require('amqplib/callback_api');
 const request = require('requestretry');
 const executor_config = require('./config.js');
 
+const availableCloudProviders = [
+    {
+        name: 'aws',
+        url: executor_config.aws_url
+    },
+    {
+        name: 'gcf',
+        url: executor_config.gcf_url
+    }
+    ];
 
+if (process.argv.length !== 3 || availableCloudProviders.find(cloudProvider => cloudProvider.name === process.argv[2]) === undefined) {
+    console.error("Incorrect usage - correct usage is 'node index.js <cloud_provider>' where <cloud_provider> could be 'aws' or 'gcf'");
+    return;
+}
+
+const cloudProvider = availableCloudProviders.find(cloudProvider => cloudProvider.name === process.argv[2]);
+
+console.log("Running for cloud provider: " + cloudProvider.name);
 
 amqp.connect('amqp://localhost', function (err, conn) {
     conn.createChannel(function (err, ch) {
@@ -11,7 +29,7 @@ amqp.connect('amqp://localhost', function (err, conn) {
         ch.assertQueue(q, {durable: true});
         console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", q);
         ch.consume(q, function (msg) {
-          //  console.log(msg.properties.replyTo);
+            //  console.log(msg.properties.replyTo);
 
             // ch.assertQueue(msg.properties.replyTo, {exclusive: true, autoDelete: true});
             // ch.sendToQueue(msg.properties.replyTo, new Buffer("YAAAAAAAAAY"), {
@@ -23,32 +41,30 @@ amqp.connect('amqp://localhost', function (err, conn) {
 
 
             function responseCallback(error, response, body) {
-               console.log("Function: " + message.executable + " status: " + response.statusCode);
+                console.log("Function: " + message.executable + " status: " + response.statusCode);
 
                 if (error) {
-                   console.log("Function: " + message.executable + " error: " + error);
-                   // hyperflow_callback(error, outs);
+                    console.log("Function: " + message.executable + " error: " + error);
+                    // hyperflow_callback(error, outs);
                     return
                 }
                 if (response) {
-                   console.log("Function: " + message.executable + " response status code: " + response.statusCode + " number of request attempts: " + response.attempts)
+                    console.log("Function: " + message.executable + " response status code: " + response.statusCode + " number of request attempts: " + response.attempts)
                 }
-               console.log("Function: " + message.executable + " data: " + body.toString());
+                console.log("Function: " + message.executable + " data: " + body.toString());
                 //hyperflow_callback(null, outs);
             }
-
 
 
             console.log("Executing:  " + JSON.stringify(message));
 
             const requestBody = {
                 timeout: 600000,
-                url: executor_config.function_trigger_url,
+                url: cloudProvider.url,
                 json: message,
                 headers: {'Content-Type': 'application/json', 'Accept': '*/*'}
             };
             request.post(requestBody, responseCallback);
-
 
 
         }, {noAck: false});
